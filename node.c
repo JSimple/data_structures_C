b3208cfd92abfc984e6a8f238683cdd3709edcd9
JSimple <josephinemayasimple@gmail.com>
2022-04-15 12:52:50 -0400

    add: make_node and fixed append_node

--- node.c
+++ node.c
@@ -1,23 +1,18 @@
-// node has:
-// value
-// pointer to next node (where it's stored?)
-
 #include <stdio.h>
+#include <stdlib.h>
 
 #define FALSE 0
 #define TRUE 1
 
 void print_list();
 void append_node();
+struct node *make_node();
 
 struct node {
     int value;
     struct node *next;
 };
 
-// struct node printValue(struct node *self){
-//     printf(self.value);
-// }
 
 int main() {
     struct node node1;
@@ -39,12 +34,8 @@ int main() {
     putchar('\n');
     append_node(&node1, 8);
     printf("after appending:\n");
-    print_list(node3);
+    print_list(node1);
 }
-
-// for next time:
-// figure out how to print next val directly
-// implement LL methods
 
 void print_list(struct node head) {
     if (head.next == NULL) {
@@ -55,23 +46,24 @@ void print_list(struct node head) {
     }
 }
 
-// LEFT OFF HERE.
-// this function doesn't seem to be appending our new node to the actual list
-
-// USE MALLOC
 void append_node(struct node *head, int new_value) {
     if (head->next == NULL) {
-        struct node new_node;
-        new_node.value = new_value;
-        new_node.next = NULL;
+        struct node *new_node = make_node(new_value);
 
-        printf("new node value to be inserted: %i\n", new_node.value);
+        printf("new node value to be inserted: %i\n", new_node->value);
 
-        head->next = &new_node;
+        head->next = new_node;
     } else {
         printf("not there yet...");
         append_node(head->next, new_value);
     }
 }
 
-// TODO: set next node function
+struct node *make_node(int new_value){
+    struct node *new_node = (struct node *) malloc(sizeof(struct node));
+    new_node->value = new_value;
+    new_node->next = NULL;
+    return new_node;
+}
+
+// TODO: insert node, prepend node, delete node
